cc1be8d58aa000fb139bb546dadd052fe80cd25a
目光所及 <Sean_390433907@163.com>
2021-11-02 11:11:46 +0800

    update

---
+++ code/BERT.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 1 2021
+
+@author: Taurids
+"""
+import time
+import torch
+import clue7
+import argparse
+from transformers import BertTokenizer, BertModel, BertConfig
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+my_parser = argparse.ArgumentParser()
+# 要求参数
+my_parser.add_argument("--model_name_or_path", default="../weights/chinese-roberta-wwm-ext", type=str, required=False)
+my_parser.add_argument("--max_seq_length", default=52, type=int, required=False)  # 文本截断长度
+my_parser.add_argument("--batch_size", default=256, type=int, required=False)
+my_parser.add_argument("--num_epochs", default=7, type=int, required=False)
+my_parser.add_argument("--learning_rate", default=5e-5, type=float, required=False)
+my_parser.add_argument("--num_labels", default=3, type=int, required=False)
+
+args = my_parser.parse_args()
+
+# 数据预处理
+tokenizer = BertTokenizer.from_pretrained(args.model_name_or_path + '/vocab.txt')
+train_set = clue7.CustomDataset('train.json', tokenizer, args.max_seq_length)
+valid_set = clue7.CustomDataset('dev.json', tokenizer, args.max_seq_length)
+test_set = clue7.CustomDataset('test_public.json', tokenizer, args.max_seq_length)
+
+# DataLoader
+train_params = {'batch_size': args.batch_size, 'shuffle': True}
+valid_params = {'batch_size': args.batch_size, 'shuffle': True}
+test_params = {'batch_size': args.batch_size, 'shuffle': False}
+train_loader = torch.utils.data.DataLoader(train_set, **train_params)
+valid_loader = torch.utils.data.DataLoader(valid_set, **valid_params)
+test_loader = torch.utils.data.DataLoader(test_set, **test_params)
+
+
+# 创建模型
+class BERTClass(torch.nn.Module):
+    def __init__(self):
+        super(BERTClass, self).__init__()
+        self.config = BertConfig.from_pretrained(args.model_name_or_path + '/config.json', output_hidden_states=True)
+        self.bert = BertModel.from_pretrained(args.model_name_or_path + '/pytorch_model.bin', config=self.config)
+        self.linear = torch.nn.Linear(768, args.num_labels)  # 分三类
+
+    def forward(self, ids, mask, token_type_ids):
+        sequence_output, pooler_output, hidden_states = self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        # [bs, len, 768]  [bs, 768]
+        output = self.linear(pooler_output)
+        return output
+
+
+net = BERTClass()
+net.to(device)
+
+# 超参数设置
+criterion = torch.nn.CrossEntropyLoss()  # 选择损失函数
+optimizer = torch.optim.Adam(net.parameters(), lr=args.learning_rate)  # 选择优化器
+
+
+# 训练模型
+clue7.train(net, train_loader, valid_loader, criterion, args.num_epochs, optimizer, device)
+
+
+# 预测函数
+def model_predict(net, test_iter):
+    # 预测模型
+    print('加载最优模型')
+    net.load_state_dict(torch.load('../model/best.pth'))
+    net.to(device)
+    print('inference测试集')
+    with torch.no_grad():
+        start = time.time()
+        test_acc, test_f1 = clue7.evaluate_accuracy(test_iter, net, device)
+        print('test acc %.3f, test f1 %.3f, time %.1f sec'
+              % (test_acc, test_f1, time.time() - start))
+
+
+# 预测
+model_predict(net, test_loader)
---
+++ code/clue7.py
@@ -0,0 +1,111 @@
+# coding=utf-8
+import torch
+import json
+from tqdm import tqdm
+from sklearn import metrics
+import time
+
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, file, tokenizer, max_len):
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+        self.ex_list = []
+        with open('../dataset/' + file, "r", encoding='utf-8') as f:
+            for line in f:
+                sample = json.loads(line)
+                query = sample["query"]
+                title = sample["title"]
+                relevant = int(sample["relevant"])
+                self.ex_list.append((query, title, relevant))
+
+    def __len__(self):
+        return len(self.ex_list)
+
+    def __getitem__(self, index):
+        query, title, relevant = self.ex_list[index]
+
+        inputs = self.tokenizer.encode_plus(
+            query, title,
+            truncation=True,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            pad_to_max_length=True,
+            return_token_type_ids=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs["token_type_ids"]
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+            'targets': torch.tensor(relevant, dtype=torch.float)
+        }
+
+
+def evaluate_accuracy(data_iter, net, device=torch.device('cpu')):
+    """Evaluate accuracy of a model on the given data set."""
+    acc_sum, n = torch.tensor([0], dtype=torch.float32, device=device), 0
+    y_pred_, y_true_ = [], []
+    for data in tqdm(data_iter):
+        # If device is the GPU, copy the data to the GPU.
+        ids = data['ids'].to(device, dtype=torch.long)
+        mask = data['mask'].to(device, dtype=torch.long)
+        token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
+        targets = data['targets'].to(device, dtype=torch.float)
+        net.eval()
+        y_hat_ = net(ids, mask, token_type_ids)
+        with torch.no_grad():
+            targets = targets.long()
+            # [[0.2 ,0.4 ,0.5 ,0.6 ,0.8] ,[ 0.1,0.2 ,0.4 ,0.3 ,0.1]] => [ 4 , 2 ]
+            acc_sum += torch.sum((torch.argmax(y_hat_, dim=1) == targets))
+            y_pred_.extend(torch.argmax(y_hat_, dim=1).cpu().numpy().tolist())
+            y_true_.extend(targets.cpu().numpy().tolist())
+            n += targets.shape[0]
+    valid_f1 = metrics.f1_score(y_true_, y_pred_, average='macro')
+    return acc_sum.item()/n, valid_f1
+
+
+def train(net, train_iter, valid_iter, criterion, num_epochs, optimizer, device):
+    print('training on', device)
+    net.to(device)
+    best_test_f1 = 0
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.9)  # 设置学习率下降策略
+    #     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=5, eta_min=2e-06)  # 余弦退火
+    for epoch in range(num_epochs):
+        train_l_sum = torch.tensor([0.0], dtype=torch.float32, device=device)
+        train_acc_sum = torch.tensor([0.0], dtype=torch.float32, device=device)
+        n, start = 0, time.time()
+        y_pred, y_true = [], []
+        for data in tqdm(train_iter):
+            net.train()
+            optimizer.zero_grad()
+            ids = data['ids'].to(device, dtype=torch.long)
+            mask = data['mask'].to(device, dtype=torch.long)
+            token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
+            targets = data['targets'].to(device, dtype=torch.float)
+            y_hat = net(ids, mask, token_type_ids)
+            loss = criterion(y_hat, targets.long())
+            loss.backward()
+            optimizer.step()
+
+            with torch.no_grad():
+                targets = targets.long()
+                train_l_sum += loss.float()
+                train_acc_sum += (torch.sum((torch.argmax(y_hat, dim=1) == targets))).float()
+                y_pred.extend(torch.argmax(y_hat, dim=1).cpu().numpy().tolist())
+                y_true.extend(targets.cpu().numpy().tolist())
+                n += targets.shape[0]
+        valid_acc, valid_f1 = evaluate_accuracy(valid_iter, net, device)
+        train_acc = train_acc_sum / n
+        train_f1 = metrics.f1_score(y_true, y_pred, average='macro')
+        print('epoch %d, loss %.4f, train acc %.3f, valid acc %.3f, '
+              'train f1 %.3f, valid f1 %.3f, time %.1f sec'
+              % (epoch + 1, train_l_sum / n, train_acc, valid_acc,
+                 train_f1, valid_f1, time.time() - start))
+        if valid_f1 > best_test_f1:
+            print('find best! save at model/best.pth')
+            best_test_f1 = valid_f1
+            torch.save(net.state_dict(), '../model/best.pth')
+        scheduler.step()  # 更新学习率
